b911c44d7105195809378ce3560ce2b26540b546
JamesKid <406358964@qq.com>
2015-09-30 10:19:26 +0800

    add push file

--- teach/push.php
+++ teach/push.php
@@ -1,3 +1,7 @@
-<?php
-	system('git pull');
+<?php  
+$www_folder = "/var/www/team/teach/teach";  
+//在这里获取到了用户提交的内容, 以及提交者等等, 可以记录到数据库中供以后使用  
+$raw_json = file_get_contents('php://input');  
+print_r(json_decode($raw_json, true));  
+echo shell_exec(" cd $www_folder ; git pull 2>&1");  
 ?>
