26f4f6e3f3c1f9be7aebe2dfb868989f38b363bf
vimkidweb <2629424100@qq.com>
2015-09-30 17:06:19 +0800

    add check whoami in push

--- teach/push.php
+++ teach/push.php
@@ -4,4 +4,5 @@ $www_folder = "/var/www/team/teach/teach";
 $raw_json = file_get_contents('php://input');  
 print_r(json_decode($raw_json, true));  
 echo shell_exec(" cd $www_folder ; git pull 2>&1");  
+//echo shell_exec("whoami 2>&1");   //查看执行命令的用户,这个用户要与nginx,apache,fpm用户一致,分配该用户执行git pull权限
 ?>
