bfaf4c2e45840656aff590581dd0f80a9256b9d9
vimkidweb <2629424100@qq.com>
2015-10-10 11:21:51 +0800

    rm note

--- teach/push.php
+++ teach/push.php
@@ -1,8 +1,8 @@
-<?php  
-$www_folder = "/var/www/team/teach/teach";  
-//在这里获取到了用户提交的内容, 以及提交者等等, 可以记录到数据库中供以后使用  
-$raw_json = file_get_contents('php://input');  
-print_r(json_decode($raw_json, true));  
-echo shell_exec(" cd $www_folder ; git pull 2>&1");  
-//echo shell_exec("whoami 2>&1");   //查看执行命令的用户,这个用户要与nginx,apache,fpm用户一致,分配该用户执行git pull权限
+<?php
+error_reporting ( E_ALL );
+$dir = '/var/www/team/teach/teach/';//该目录为git检出目录
+$handle = popen('cd '.$dir.' && git pull 2>&1','r');
+$read = stream_get_contents($handle);
+printf($read);
+pclose($handle);
 ?>
