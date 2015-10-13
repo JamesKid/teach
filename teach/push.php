4406b6d36fe18f8c84fd35ba12265575f0be6d31
JamesKid <406358964@qq.com>
2015-10-13 10:30:18 +0800

    rm push.php

--- teach/push.php
+++
@@ -1,9 +0,0 @@
-<?php
-set_time_limit(1800);
-error_reporting ( E_ALL );
-$dir = '/var/www/team/teach/teach/';//该目录为git检出目录
-$handle = popen('cd '.$dir.' && git pull 2>&1','r');
-$read = stream_get_contents($handle);
-printf($read);
-pclose($handle);
-?>
